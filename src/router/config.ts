3b10a9c4652670eaca0c958a47f2a0003548905d
alxdrcs <854461409@qq.com>
2022-03-27 00:51:53 +0800

    配置alias

--- src/router/config.ts
+++ src/router/config.ts
@@ -2,6 +2,6 @@ import { RouteRecordRaw } from 'vue-router'
 
 export const routes: RouteRecordRaw[] = [
   { path: '/', redirect: '/home' },
-  { path: '/home', component: () => import('../views/home/index.vue') },
-  { path: '/login', component: () => import('../views/login/Index.vue') }
+  { path: '/home', component: () => import('@/views/home/index.vue') },
+  { path: '/login', component: () => import('@/views/login/Index.vue') }
 ]
