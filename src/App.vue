5ad00d3d5dcfbddd4ac869554abb8ba6fda89977
alxdrcs <854461409@qq.com>
2022-03-27 00:22:35 +0800

    添加tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,7 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <h1>1</h1>
+  <h1 class="text-center">1</h1>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
